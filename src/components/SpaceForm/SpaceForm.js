306221851ed0f49ff3e443ae4b4dfeaea5e2db97
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-06-14 12:18:03 +0300

    Uncomment forgotten yup tests

--- src/components/SpaceForm/SpaceForm.js
+++ src/components/SpaceForm/SpaceForm.js
@@ -44,53 +44,53 @@ export const SpaceForm = withFormik({
   },
 
   validationSchema: yup.object().shape({
-    // firstNameCyrillic: yup
-    //   .string()
-    //   .test("isCyrillic", "Заполняйте кириллицей", val => isCyrillic(val))
-    //   .required("Обязательно к заполнению"),
-    // firstNameLatin: yup.string().required("Обязательно к заполнению"),
-    // lastNameCyrillic: yup.string().required("Обязательно к заполнению"),
-    // lastNameLatin: yup.string().required("Обязательно к заполнению"),
-    // email: yup
-    //   .string()
-    //   .email("Недопустимый формат")
-    //   .required("Обязательно к заполнению"),
-    // phoneNumber: yup.string().required("Обязательно к заполнению"),
-    // personalPhoto: yup
-    //   .mixed()
-    //   .required("Обязательно к заполнению")
-    //   .test(
-    //     "fileSize",
-    //     "Файл должен быть не более 320kb",
-    //     value => value && value.size <= FILE_SIZE
-    //   )
-    //   .test(
-    //     "fileFormat",
-    //     "Только изображения в формате jpg/jpeg/gif/png",
-    //     value => value && SUPPORTED_FORMATS.includes(value.type)
-    //   ),
-    // rocketPhoto: yup
-    //   .mixed()
-    //   .required("Обязательно к заполнению")
-    //   .test(
-    //     "fileSize",
-    //     "Файл должен быть не более 320kb",
-    //     value => value && value.size <= FILE_SIZE
-    //   )
-    //   .test(
-    //     "fileFormat",
-    //     "Только изображения в формате jpg/jpeg/gif/png",
-    //     value => value && SUPPORTED_FORMATS.includes(value.type)
-    //   ),
-    // nameOfRocket: yup.string().required("Обязательно к заполнению"),
+    firstNameCyrillic: yup
+      .string()
+      .test("isCyrillic", "Заполняйте кириллицей", val => isCyrillic(val))
+      .required("Обязательно к заполнению"),
+    firstNameLatin: yup.string().required("Обязательно к заполнению"),
+    lastNameCyrillic: yup.string().required("Обязательно к заполнению"),
+    lastNameLatin: yup.string().required("Обязательно к заполнению"),
+    email: yup
+      .string()
+      .email("Недопустимый формат")
+      .required("Обязательно к заполнению"),
+    phoneNumber: yup.string().required("Обязательно к заполнению"),
+    personalPhoto: yup
+      .mixed()
+      .required("Обязательно к заполнению")
+      .test(
+        "fileSize",
+        "Файл должен быть не более 320kb",
+        value => value && value.size <= FILE_SIZE
+      )
+      .test(
+        "fileFormat",
+        "Только изображения в формате jpg/jpeg/gif/png",
+        value => value && SUPPORTED_FORMATS.includes(value.type)
+      ),
+    rocketPhoto: yup
+      .mixed()
+      .required("Обязательно к заполнению")
+      .test(
+        "fileSize",
+        "Файл должен быть не более 320kb",
+        value => value && value.size <= FILE_SIZE
+      )
+      .test(
+        "fileFormat",
+        "Только изображения в формате jpg/jpeg/gif/png",
+        value => value && SUPPORTED_FORMATS.includes(value.type)
+      ),
+    nameOfRocket: yup.string().required("Обязательно к заполнению"),
     engineСapacity: yup
       .number()
       .typeError("Только числовые значения")
       .positive("Только позитивные значения")
       .required("Обязательно к заполнению"),
-    //   backFromBlackHole: yup.boolean(),
-    //   chanceToBack: yup.string(),
-    //   haveRecord: yup.string().required("Обязательно к заполнению"),
+    backFromBlackHole: yup.boolean(),
+    chanceToBack: yup.string(),
+    haveRecord: yup.string().required("Обязательно к заполнению"),
     loadSpeed: yup
       .number()
       .typeError("Только числовые значения")
@@ -99,9 +99,9 @@ export const SpaceForm = withFormik({
   }),
   handleSubmit: (values, { setSubmitting, setValues }) => {
     setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      // const payload = { ...values, modalIsVisible: true };
-      // setValues(payload);
+      // alert(JSON.stringify(values, null, 2));
+      const payload = { ...values, modalIsVisible: true };
+      setValues(payload);
       setSubmitting(false);
     }, 1000);
   }
