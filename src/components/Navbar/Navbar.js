ad2a6cb0e40dc904b5fb16ea9fa9e8148dc25618
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2019-06-14 10:45:06 +0300

    Change BrowserRouter to HashRouter

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Home from "../Home/Home";
 import RenderLists from "../RenderLists/RenderLists";
 import { SpaceForm } from "../SpaceForm/SpaceForm";
 import "./Navbar.css";
 
 const Navbar = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <div className="navbar">
       <Link to="/">Home</Link>
       <Link to="/render-lists">Render List</Link>
